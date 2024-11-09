509b7b68de82ef6d5d114937f59b552ad1112bf6
Arun Singh <arun@credlawn.com>
2024-11-10 02:02:11 +0530

    Campaign Module Added

--- credlawn/credlawn/doctype/blasting/blasting.py
+++ credlawn/credlawn/doctype/blasting/blasting.py
@@ -1,3 +1,4 @@
+import frappe
 from frappe import _
 from frappe.model.document import Document
 
@@ -13,3 +14,18 @@ class Blasting(Document):
         # New validation for app_status
         if self.login_status in ["VKYC", "SVKYC", "BKYC", "Risk", "Cure Done", "Approved"]:
             self.app_status = "Complete"
+
+    def after_insert(self):
+        campaign_data = frappe.db.get_value("Campaigndata", self.name, ["campaign_type", "campaign_date", "customer_name", "mob_no", "data_source"], as_dict=True)
+
+        if campaign_data:
+            # Assign values from the Campaign Data to the Blasting document
+            self.campaign_type = campaign_data.campaign_type
+            self.campaign_date = campaign_data.campaign_date
+            self.customer_name = campaign_data.customer_name
+            self.mobile_no = campaign_data.mob_no
+            self.data_source = campaign_data.data_source
+            self.save(ignore_permissions=True)
+            frappe.enqueue('credlawn.scripts.send_lead.send_lead', blasting_name=self.name)
+        else:
+            return

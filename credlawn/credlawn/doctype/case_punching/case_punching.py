6238b4ca6e48e6162218a46548b2d70136cb5700
Arun Singh <arun@credlawn.com>
2024-11-12 11:30:06 +0530

    Campaign Module Updated and SMS Campaign Added

--- credlawn/credlawn/doctype/case_punching/case_punching.py
+++ credlawn/credlawn/doctype/case_punching/case_punching.py
@@ -1,9 +1,28 @@
-# Copyright (c) 2024, Credlawn India Private Limited and contributors
-# For license information, please see license.txt
-
-# import frappe
+import frappe
 from frappe.model.document import Document
-
+import re
 
 class CasePunching(Document):
-	pass
+    def validate(self):
+        reference_pattern = r"^24[A-Z]\d{2}[A-Z]\d{8}[A-Z0-9]{2}$"
+        mobile_pattern = r"^\d{10}$"
+        
+        if self.reference_no:
+            if not re.match(reference_pattern, self.reference_no):
+                frappe.throw("Invalid Reference No. Please try Again")
+        else:
+            frappe.throw("Reference is Missing. Please enter Reference No.")
+        
+        if self.mobile_no:
+            if not re.match(mobile_pattern, self.mobile_no):
+                frappe.throw("Mobile Number must be 10 digits.")
+        else:
+            frappe.throw("Mobile Number is Missing. Please enter Mobile No.")
+
+    def after_insert(self):
+
+        if self.reference_no:
+            self.reference_no = self.reference_no.upper().strip()
+
+        if self.customer_name:
+            self.customer_name = self.customer_name.title().strip()
